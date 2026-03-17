fd21a609d73956c945235a5db867be2f39161066
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-03-17 12:57:12 +0530

    Updated Fix

--- server/src/invoices/invoices.module.ts
+++ server/src/invoices/invoices.module.ts
@@ -3,7 +3,7 @@ import {MongooseModule} from '@nestjs/mongoose';
 import { InvoicesService } from './invoices.service';
 import { InvoicesController } from './invoices.controller';
 import {Invoice, InvoiceSchema } from './schemas/invoice-schema';
-import { ItemsModule } from 'src/items/items.module';
+import { ItemsModule } from '../items/items.module';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Invoice.name, schema: InvoiceSchema }]),
