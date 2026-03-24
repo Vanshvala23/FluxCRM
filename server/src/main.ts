63cdee8300c0fac4c41d894841a4e3281e4a08c0
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-03-24 12:43:25 +0530

    Updated

--- server/src/main.ts
+++ server/src/main.ts
@@ -47,7 +47,14 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api/docs', app, document, {
+  swaggerOptions: { persistAuthorization: true },
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.min.js',
+  ],
+});
 
   await app.listen(process.env.PORT || 4000);
   console.log(`Application is running on: http://localhost:${process.env.PORT || 4000}`);
