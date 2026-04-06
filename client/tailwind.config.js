9f857dbbdfd5f8d7ade35a5d81747f90071eedae
VanshVala <102245923+Vanshvala23@users.noreply.github.com>
2026-04-06 19:00:24 +0530

    Updated UI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,29 +1,26 @@
-import { heroui } from "@heroui/react";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './index.html','./src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       colors: {
         primary: {
-          50: "#eef2ff",
-          100: "#e0e7ff",
-          200: "#c7d2fe",
-          300: "#a5b4fc",
-          400: "#818cf8",
-          500: "#6366f1",
-          600: "#4f46e5", // ✅ THIS FIXES YOUR ERROR
-          700: "#4338ca",
-          800: "#3730a3",
-          900: "#312e81",
+          50: '#eef2ff',
+          100: '#e0e7ff',
+          200: '#c7d2fe',
+          300: '#a5b4fc',
+          400: '#818cf8',
+          500: '#6366f1',
+          600: '#4f46e5',
+          700: '#4338ca',
+          800: '#3730a3',
+          900: '#312e81',
         },
       },
     },
   },
-  plugins: [heroui()],
-};
+  plugins: [],
+}
+
